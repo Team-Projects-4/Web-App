537891bddb7d458f8b45035a7ad2e88c5954ed38
monicabrutto <brutto.2@wright.edu>
2025-03-25 08:56:42 -0400

    Moved imgs to correct folder

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -33,15 +33,15 @@ class _DeathStarPlansPageState extends State<DeathStarPlansPage> {
   final double slideHeight = 720.0;
   final List<String> images = [
     'assets/images/image1.png',
-    // 'assets/images/image2.png',
-    // 'assets/images/image3.png',
-    // 'assets/images/image4.png',
-    // 'assets/images/image5.png',
-    // 'assets/images/image6.png',
-    // 'assets/images/image7.png',
-    // 'assets/images/image8.png',
-    // 'assets/images/image9.png',
-    // 'assets/images/image10.png',
+    'assets/images/image2.png',
+    'assets/images/image3.png',
+    'assets/images/image4.png',
+    'assets/images/image5.png',
+    'assets/images/image6.png',
+    'assets/images/image7.png',
+    'assets/images/image8.png',
+    'assets/images/image9.png',
+    'assets/images/image10.png',
 
   ];
 
